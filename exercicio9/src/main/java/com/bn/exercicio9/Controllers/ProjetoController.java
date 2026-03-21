63e83d448a4c5cdd23ba67c7ca7b51af293a182e
VihSouza06 <visouzasantos2006@gmail.com>
2026-03-21 16:16:55 -0300

    Criação do Controller

---
+++ exercicio9/src/main/java/com/bn/exercicio9/Controllers/ProjetoController.java
@@ -0,0 +1,51 @@
+package com.bn.exercicio9.Controllers;
+
+import com.bn.exercicio9.Models.ProjetoModel;
+import com.bn.exercicio9.Services.ProjetoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("/projetos")
+public class ProjetoController {
+
+    @Autowired
+    private ProjetoService projetoService;
+
+    @GetMapping
+    public ResponseEntity<List<ProjetoModel>> findAll(){
+        List<ProjetoModel> projeto = projetoService.findAll();
+        return ResponseEntity.ok().body(projeto);
+    }
+
+    @GetMapping("/{id}")
+    public ProjetoModel buscarProjetoPorId(@PathVariable Long id){
+        return projetoService.buscarProjetoPorId(id);
+    }
+
+    @PostMapping
+    public ResponseEntity<ProjetoModel> criarProjeto(@RequestBody ProjetoModel projetoModel){
+        ProjetoModel projetoCriado = projetoService.criarProjeto(projetoModel);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri()
+                .path("/{id}").buildAndExpand(projetoModel.getId()).toUri();
+        return ResponseEntity.created(uri).body(projetoCriado);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deletarProjeto(@PathVariable Long id){
+        projetoService.deletarProjeto(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ProjetoModel> atualizarProjeto
+            (@PathVariable Long id, @RequestBody ProjetoModel projetoModel){
+        ProjetoModel projetoAtualizada = projetoService.atualizarProjeto(id, projetoModel);
+        return ResponseEntity.ok(projetoAtualizada);
+    }
+}
